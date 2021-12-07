45d9f809a2ae6e712806ae83c97c6fa2771e74c4
Vishwajit1997 <vishwajitkumar37@gmail.com>
2021-12-07 17:41:35 +0530

    Updated as Name String

---
+++ String/Name.java
@@ -0,0 +1,8 @@
+public class Name {
+
+    public static void main(String[] args)
+    {
+        String text = "Vishwajit Tiwari";
+        System.out.println(text);
+    }
+}
--- String/String.java
+++
@@ -1,8 +0,0 @@
-public class String {
-
-    public static void main(String[] args)
-    {
-        String Name = "Vishwajit";
-        System.out.println(Name);
-    }
-}
