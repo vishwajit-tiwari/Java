6db305c5919cdbf6358cdb289359d979e5ad2e3a
Vishwajit1997 <vishwajitkumar37@gmail.com>
2021-12-07 16:23:46 +0530

    Added Variables Code

---
+++ Variables/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    public static void main(String[] args)
+    {
+        int aValue = 10;
+        //aValue = 10;
+        short aShort = 98;
+        long aLong = 6432;
+        double aDouble = 4.2452;
+        float aFloat = 6.2425f;
+        char aChar = 'V';
+
+        boolean  aBoolean = true;
+        byte aByte = 127;
+
+        System.out.println(aValue);
+        System.out.println(aShort);
+        System.out.println(aLong);
+        System.out.println(aDouble);
+        System.out.println(aFloat);
+        System.out.println(aChar);
+        System.out.println(aBoolean);
+        System.out.println(aByte);
+    }
+}
